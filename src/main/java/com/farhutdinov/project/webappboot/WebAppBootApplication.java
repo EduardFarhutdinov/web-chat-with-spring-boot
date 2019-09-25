cf28c70a8f8db8f94b5959b15dac06bb670e41fe
Eduard Farkhutdinov <farhutdinoveduard16@gmail.com>
2019-09-25 19:32:15 +0300

    create project

---
+++ src/main/java/com/farhutdinov/project/webappboot/WebAppBootApplication.java
@@ -0,0 +1,13 @@
+package com.farhutdinov.project.webappboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/farhutdinov/project/webappboot/WebAppBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.farhutdinov.project.webappboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebAppBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
